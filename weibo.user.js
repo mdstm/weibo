5498b24d4aa0bcbe77cb990caccff589858cf826
mdstm <70753840+mdstm@users.noreply.github.com>
2023-01-07 17:01:01 +0800

    Update 7.0 (只能下载视频和 LIVE)

--- weibo.user.js
+++ weibo.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         微博图片下载脚本
 // @homepage     https://github.com/mdstm/weibo
-// @version      6.1
-// @description  下载旧版微博网页版的图片和视频
+// @version      7.0
+// @description  下载微博网页版的图片和视频
 // @author       mdstm
 // @match        https://weibo.com/*
 // @match        https://www.weibo.com/*
@@ -20,86 +20,89 @@
   'use strict';
 
   /**
-   * 设置下载名称
-   * @param  {object} info 微博信息
-   * @param  {number} idx  序号
-   * @param  {string} ext  扩展名，不带点
-   * @return {string}      下载名称
-   */
-  function setName(info, idx, ext) {
-    let t = new Date(info.created_at);
-    t.setTime(t.getTime() - t.getTimezoneOffset() * 60000 + idx * 1000);
-    t = t.toISOString().substring(2, 19);
-    return t.replace(/[-:]/g, '').replace('T', '_') + '.' + ext;
-  }
-
-  /**
    * 下载
-   * @param {string} url  链接
-   * @param {string} name 下载名称
    */
   function download(url, name) {
     GM_download({
       url: url,
       name: name,
-      headers: [{name: 'referer', value: 'https://weibo.com/'}],
+      headers: [
+        {name: 'referer', value: 'https://weibo.com/'}
+      ],
       onerror: function() { console.error('下载 ' + name + ' 失败\n' + url); },
-      ontimeout: function() { download(url, name); }
+      ontimeout: function() { console.error('下载 ' + name + ' 超时\n' + url); }
     });
+  }
+
+  /**
+   * 计算本地文件的名称
+   */
+   function getName(date, idx, ext) {
+    let t = new Date(date.getTime() + idx * 1000).toISOString();
+    t = t.substring(2, 19).replace(/[-:]/g, '').replace('T', '_');
+    return t + ext;
+  }
+
+  /**
+   * 从多个路径中选出第一个可达到的目标
+   */
+  function choice(obj, paths) {
+    for (let path of paths) {
+      try {
+        let a = obj;
+        for (let k of path) {
+          a = a[k];
+        }
+        return a;
+      } catch (e) {
+        return null;
+      }
+    }
   }
 
   /**
    * 下载图片和视频
    */
-  function downPicture(info) {
-    let url, idx = 0, ext;
+  function downMedia(info) {
+    let date = new Date(info.created_at); // 微博创建时间
+    date.setTime(date.getTime() - date.getTimezoneOffset() * 60000); // 减去时区偏移
 
-    if (info.pic_infos) { // 普通图片
-      console.log('找到普通图片，开始下载图片');
-      for (let pic of Object.values(info.pic_infos)) {
-        url = pic.largest.url;
-        url = url.replace(/\/wx[123]\./, '/wx4.');
-        ext = url.match(/\w+$/)[0];
-        download(url, setName(info, idx++, ext)); // 下载图片
-        if (ext != 'gif' && pic.video) {
-          download(pic.video, setName(info, idx++, 'mp4')); // 下载 LIVE
+    // 有三种微博，图片微博、视频微博和文字微博，每个微博只能属于其中一种
+
+    // 如果该项存在, 则是图片微博
+    let pic_infos = info.pic_infos;
+    if (pic_infos) {
+      console.log('找到图片');
+      let idx = 0; // 文件序号
+      for (let pic of Object.values(pic_infos)) { // 遍历图片
+        let url = pic.largest.url; // 图片链接
+        let ext = url.match(/\.\w+$/)[0]; // 图片扩展名，目前只发现有 .jpg 和 .gif
+        download(url, getName(date, idx++, ext)); // 下载图片
+        if (ext != '.gif') {
+          let video = pic.video;
+          if (video) { // 如果非 gif 图片存在该视频链接，则有 LIVE
+            download(video, getName(date, idx++, '.mp4')); // 下载 LIVE
+          }
         }
       }
       return;
     }
 
-    if (info.page_info) {
-      let page_info = info.page_info;
-
-      if (page_info.media_info) { // 普通视频
-        let media_info = page_info.media_info;
-        try {
-          url = media_info.playback_list[0].play_info.url;
-        } catch (e) {}
-        if (url
-        || (url = media_info.mp4_720p_mp4)
-        || (url = media_info.mp4_hd_url)
-        || (url = media_info.mp4_sd_url)) {
-          console.log('找到普通视频，开始下载视频');
-          download(url, setName(info, 0, 'mp4')); // 下载视频
-          return;
-        }
+    // 如果该项存在，则有可能是视频微博
+    let page_info = info.page_info;
+    if (page_info) {
+      let url = choice(page_info, [
+        ['media_info', 'playback_list', 0, 'play_info', 'url'], // 普通视频
+        ['media_info', 'mp4_720p_mp4'],
+        ['media_info', 'mp4_hd_url'],
+        ['media_info', 'mp4_sd_url'],
+        ['slide_cover', 'playback_list', 0, 'play_info', 'url'] // 微博故事
+      ]);
+      if (url) {
+        console.log('找到视频');
+        download(url, getName(date, 0, '.mp4')); // 下载视频
+        return;
       }
-
-      try { // 微博故事
-        url = page_info.slide_cover.playback_list[0].play_info.url.toString();
-        console.log('找到微博故事，开始下载视频');
-        download(url, setName(info, 0, 'mp4')); // 下载视频
-        return;
-      } catch (e) {}
-
-      try { // 明星动态
-        url = page_info.card_info.pic_url.toString();
-        ext = url.match(/\w+$/)[0];
-        console.log('找到明星动态，开始下载图片');
-        download(url, setName(info, 0, ext)); // 下载图片
-        return;
-      } catch (e) {}
     }
 
     console.log('未找到图片或视频');
@@ -126,63 +129,31 @@
           return;
         }
         console.log('获取全部信息成功');
-        downPicture(info);
+        downMedia(info);
       }
     });
   }
-
-  // /**
-  //  * 判断节点所在微博是否有图片或视频
-  //  */
-  // function isGood(a) {
-  //   try {
-  //     let box = a.parentNode.parentNode;
-
-  //     if (!a.href.match(/\d+\/\w+/)) { // 微博链接不正确
-  //       return false;
-  //     }
-
-  //     if (box.classList.contains('WB_func')) { // 是被转发微博
-  //       box = box.parentNode;
-  //     } else if (box.querySelector('.WB_feed_expand')) { // 是转发微博
-  //       return false;
-  //     }
-
-  //     if (box.querySelector('.WB_pic') // 存在图片
-  //     || box.querySelector('.WB_video')) { // 存在视频
-  //       return true;
-  //     }
-  //     return false;
-  //   } catch (e) {
-  //     return false;
-  //   }
-  // }
 
   /**
    * 初始化，给微博卡片增加按钮
    */
   function init() {
-    let aList = document.querySelectorAll( // 列出未被检测的节点
-      // '.WB_from>a[node-type="feed_list_item_date"]:not(.mdstm)'
+    let a_list = document.querySelectorAll( // 列出未被检测的节点
       'a.head-info_time_6sFQg:nth-child(1):not(.mdstm)'
     );
 
-    for (let a of aList) {
+    for (let a of a_list) {
       a.className += ' mdstm'; // 添加已检测标记
-      // if (!isGood(a)) {
-      //   continue;
-      // }
       let b = document.createElement('a'); // 创建下载按钮
       b.className = 'head-info_time_6sFQg';
       b.style.cursor = 'pointer';
       b.innerHTML = '下载';
       b.onclick = click;
 
-      // a.parentNode.appendChild(b);
       a.parentNode.insertBefore(b, a.nextSibling);
     }
 
-    let n = aList.length;
+    let n = a_list.length;
     if (n > 0) {
       console.log('添加了 ' + n + ' 个按钮');
     }
